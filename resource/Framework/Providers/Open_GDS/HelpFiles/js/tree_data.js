dbdf3ee5453b8e9cd4a9f09df0f5c197a5925841
Bas van Etten <bas.v.etten@gmail.com>
2026-02-10 17:18:22 +0100

    Updated the help files

---
+++ resource/Framework/Providers/Open_GDS/HelpFiles/js/tree_data.js
@@ -0,0 +1,39 @@
+const TOC_DATA = [
+    {
+        "text": "Getting Started with GOOP Development Suite",
+        "href": "GettingStartedwithGOOPDevelopm5.html",
+        "children": [
+            { "text": "Finding the features", "href": "Findingthefeatures5.html" },
+            { "text": "Create New Class", "href": "CreateNewClass5.html" },
+            { "text": "Create Design Pattern", "href": "CreateDesignPattern5.html" },
+            { "text": "Create New Method", "href": "CreateNewMethod5.html" },
+            { "text": "Create VI Icon", "href": "CreateVIIcon5.html" },
+            { "text": "Platform Compatibility", "href": "PlatformCompatibility.html" }
+        ]
+    },
+    {
+        "text": "Class Provider Options",
+        "href": "GOOPOptionsDialog5.html",
+        "children": [
+            { "text": "Endevo GOOP 4 Class Template", "href": "EndevoGOOP4ClassTemplate5.html" },
+            { "text": "Endevo GOOP 3 Class Template", "href": "EndevoGOOP3ClassTemplate5.html" },
+            { "text": "Change or Add Methods in a Class Template", "href": "ChangeorAddMethodsinaClassTemp5.html" },
+            { "text": "Create Class Template", "href": "CreateClassTemplate5.html" }
+        ]
+    },
+    { "text": "GOOP Debugger", "href": "GOOPDebugger5.html" },
+    { "text": "GOOP 4 vs GOOP 3", "href": "GOOP4vsGOOP35.html" },
+    { "text": "GOOP 3 vs GOOP 2", "href": "GOOP3vsGOOP25.html" },
+    {
+        "text": "UML Modeller",
+        "href": "UMLModeller5.html",
+        "children": [
+            { "text": "Class Diagram", "href": "ClassDiagram5.html" },
+            { "text": "Sequence Diagram", "href": "SequenceDiagram5.html" },
+            { "text": "Use Case Diagram", "href": "UseCaseDiagram5.html" },
+            { "text": "Package Diagram", "href": "PackageDiagram5.html" },
+            { "text": "State Diagram", "href": "StateDiagram5.html" }
+        ]
+    },
+    { "text": "Demo Videos", "href": "Demos5.html" }
+];
